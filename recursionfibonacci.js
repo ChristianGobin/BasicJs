83e834a550992d81e95c8ac7624c77ad3db60f0b
Christian G <rootcgvpn@gmail.com>
2018-07-27 13:52:10 -0400

    comment fixed

--- recursionfibonacci.js
+++ recursionfibonacci.js
@@ -7,5 +7,6 @@ function fib(num){
 	}
 	return fib(num - 1) + fib(num - 2);
 }
-// replace # with nth position of sequence to find value you are looking for. Ex the value of the 3rd position = console.log(3));
+// replace # with nth position of sequence to find value you are looking for. Ex the value of the 3rd position = console.log(fib(3));
+
 console.log(fib(#));
