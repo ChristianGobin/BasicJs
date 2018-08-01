30ae36d7f152bf47fdb9b493589640d8b204a2ce
Christian G <rootcgvpn@gmail.com>
2018-07-31 20:18:31 -0400

    factorial - oddEven
    wrote 2 functions

--- functions.js
+++ functions.js
@@ -1,1 +1,19 @@
-
+console.log("factorial function");
+function factorial(x){
+  var result = 1;
+  for (var i = 2 ; i <= x ; i++){
+    result = i * result; }
+  return result;
+}
+// replace # with desired number to find its factorial
+console.log(factorial(#));                     
+//--------------------------------------------------------------
+console.log("Even or Odd Funtion");
+function oddEven(y){
+  if(y % 2 == 0){
+    console.log("Even") }
+  else{
+    console.log("Odd"); }
+}
+//replace # with number to be tested
+console.log(oddEven(#));
