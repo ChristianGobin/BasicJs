403eb686e592d163c02aefddbddcc2a8dc7c1ce3
Christian G <rootcgvpn@gmail.com>
2018-07-31 21:03:26 -0400

    add character Swap function

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ function factorial(x){
   return result;
 }
 // replace # with desired number to find its factorial
-factorial(#)                
+console.log(factorial(#));                
 //--------------------------------------------------------------
 console.log("Even or Odd Funtion");
 function oddEven(y){
@@ -16,4 +16,13 @@ function oddEven(y){
     console.log("Odd"); }
 }
 //replace # with number to be tested
-oddEven(#)
+oddEven(#);
+//--------------------------------------------------------------
+console.log("Replace Characters in a String");
+function replacement(str){
+               //str.replace(/character to be swap out/g , "character to swap in");
+  var newStr = str.replace(/-/g , "_");
+  return newStr;
+}
+console.log(replacement("This-is-a-string");
+//output will replace all "-"'s with "_"           
