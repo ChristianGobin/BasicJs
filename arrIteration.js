7b0028e1494d5dcd7c48e413eb3a9dc6eed7895c
Christian G <rootcgvpn@gmail.com>
2018-08-29 16:20:36 -0400

    creation
    variety of functions for array iteration.

---
+++ arrIteration.js
@@ -0,0 +1,16 @@
+//find max number in array.
+function max(arr){
+  var x = arr[0];
+  for(i = 1; i < arr.length; i++){
+    if(x < arr[i]){
+      x = arr[i]; }
+   }
+   return x; 
+}
+// in progress
+//sum of an array
+function sumArr(arr){
+   var x = arr[0];
+   arr.forEach(function(x){
+   
+}
