0c181ae1f4716f6fd7f2eb00a927a0f60a471a0d
Christian G <rootcgvpn@gmail.com>
2018-08-29 22:27:03 -0400

    added more functions

--- arrIteration.js
+++ arrIteration.js
@@ -7,10 +7,24 @@ function max(arr){
    }
    return x; 
 }
-// in progress
+
 //sum of an array
 function sumArr(arr){
-   var x = arr[0];
+   var total = 0;
    arr.forEach(function(x){
-   
+   total+=x;
+   });
+	return(total);
+}
+
+//find if array is uniform
+function isUniform (arr){
+	var x = arr[0];
+	for(i=1;i<arr.length;i++){
+		if(x == i){
+			console.log(true);
+		} if(x!=i){
+			console.log(false);
+		}
+	}
 }
