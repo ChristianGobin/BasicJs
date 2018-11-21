bbdc1e6c1c8445d835cd34e7a4a682c36b29e38b
Christian G <chris.gobin@proximite.space>
2018-11-21 15:41:37 -0500

    optimized fizzbuzz
    First fizzbuzz was hacked together. optibuzz performs the task in a simpler dry style of code.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 // Javascript Fizzbuzz Function. Numbers 1 - 100
-
+//Original Fizzbuzz hacked together 
 function fizzbuzz (){
     for(var i = 0; i <= 100; i++){
         if(i%3===0 && i%5===0){
@@ -9,5 +9,19 @@ function fizzbuzz (){
         else if (i%5===0&&i%3!=0){
             console.log('buzz');}
         else { console.log(i);}
+    }
+}
+
+
+//optimized fizzbuzz 
+function optibuzz(){
+    for(var i=0; i<=100; i++){
+        //statement variable used to store strings fizz, buzz or both after conditions are executed.
+        var statement = ("");
+        if(i % 3 === 0){statement += 'fizz';}
+        if(i % 5 === 0){statement += 'buzz';}
+        //statement does not have fizz or buzz inside thus it failed the conditional tests and will return the integer
+        if(statement == ("")){statement = i;}
+		console.log(statement);
     }
 }
